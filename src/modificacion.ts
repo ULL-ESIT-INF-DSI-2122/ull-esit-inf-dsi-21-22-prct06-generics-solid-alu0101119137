64b1db11a2122584b775a6e501549e91af44780a
Andrea Hernandez <alu0101119137@ull.edu.es>
2022-03-21 10:45:13 +0000

    parte modificacion

---
+++ src/modificacion.ts
@@ -0,0 +1,102 @@
+/**
+ * Modificación
+ * @module Modificación
+ * @author Andrea Hernández Martín
+ */
+
+
+/**
+ * Interfaz Collectable de tipo generico T
+ */
+interface Collectable<T> {
+  addItem(newItem: T): void;
+  getItem(indice: number): T;
+  removeItem(indice: number): void;
+  getNumberOfItems(): number;
+}
+
+/**
+ * Interfaz genérica Printable
+ */
+interface Printable {
+  print(): void;
+}
+
+/**
+ * Clase abstracta PrintableCollection de tipo genérico
+ */
+abstract class PrintableCollection<T> implements Collectable<T>, Printable {
+  /**
+   * Constructor de la clase abstracta PrintableCollection
+   * @param items array de valores de la clase
+   */
+  constructor(protected items: T[]) {
+  }
+
+  /**
+   * Función que añade un item nuevo
+   * @param newItem item que se añade al array de items
+   */
+  addItem(newItem: T): void {
+    this.items.push(newItem);
+  }
+
+  /**
+   * Función que devuelve el item según el índice
+   * @param indice Lugar donde se encuentra el item a buscar
+   * @returns Devuleve el item que se encuentra en el lugar del índice que se especifica
+   */
+  getItem(indice: number): T {
+    return this.items[indice];
+  }
+
+  /**
+   * Función que elimina un item de la posision que se le pasa por índice
+   * @param indice Posision del item a eliminar
+   */
+  removeItem(indice: number): void {
+    this.items.splice(indice, 1);
+  }
+
+  /**
+   * Función que devuelve el numero de items total que hay
+   * @returns El numero de items en el array
+   */
+  getNumberOfItems(): number {
+    return this.items.length;
+  }
+
+  /**
+   * Función print que muestra una cadena con todos los items
+   */
+  abstract print(): void;
+}
+
+/**
+ * Clase NumericPrintableCollection que hereda de la clase PrintableCollection
+ */
+export class NumericPrintableCollection extends PrintableCollection<number> {
+  /**
+   * Constructror de la clase
+   * @param items array de items
+   */
+  constructor(items: number[]) {
+    super(items);
+  }
+
+  /**
+   * Funcion que imprime la colección de valores
+   */
+  print(): void {
+    const it: number[] = this.items;
+    const arrStr: string[] = [];
+    // let result: string = '';
+    it.forEach((element) => {
+      arrStr.push((element.toString()));
+    });
+    console.log(arrStr.join(','));
+  }
+}
+
+const num = new NumericPrintableCollection([1, 2]);
+num.print();
---
+++ tests/modificacion.spec.ts
@@ -0,0 +1,23 @@
+import 'mocha';
+import {expect} from 'chai';
+import {NumericPrintableCollection} from '../src/modificacion';
+
+describe('Test de la clase NumericPrintableCollection', () => {
+  const miColeccion = new NumericPrintableCollection([1, 3, 5]);
+
+  it('Se crea correctamente la clase NumericPrintableCollection', () => {
+    expect(new NumericPrintableCollection([1, 3, 5]));
+  });
+  it('Se añade correctamente un item', () => {
+    expect(miColeccion.addItem(4));
+  });
+  it('Se obtiene un item segun una posicion correctamente', () => {
+    expect(miColeccion.getItem(1)).to.be.equal(3);
+  });
+  it('Se elimina un item segun una posicion correctamente', () => {
+    expect(miColeccion.removeItem(1));
+  });
+  it('Se obtiene el número total de items correctamente', () => {
+    expect(miColeccion.getNumberOfItems()).to.be.equal(3);
+  });
+});
