bdad3d4905ebb440eaa91f2c67c74955387e6e54
Andrea Hernandez <alu0101119137@ull.edu.es>
2022-03-21 13:22:39 +0000

    clase marvel

--- src/ejercicio-1/marvel.ts
+++ src/ejercicio-1/marvel.ts
@@ -1,27 +1,27 @@
 /**
  * Ejercicio 1. El combate definitivo
- * @module Pokemon
+ * @module Marvel
  * @author Andrea Hernández Martín
  */
 
- import {Fighter} from './fighter';
- import {DatosBasicos} from './fighter';
- import {Universo} from './fighter';
- 
- /**
-  * Clase Pokemon
+import {Fighter} from './fighter';
+import {DatosBasicos} from './fighter';
+import {Universo} from './fighter';
+
+/**
+* Clase Marvel
+*/
+export class Marvel extends Fighter {
+  /**
+  * Constructor de la clase Marvel
+  * @param nombre nombre del luchador de Marvel
+  * @param peso peso del luchador de Marvel
+  * @param altura altura del luchador de Marvel
+  * @param datos datos de ataque, velocidad, defensa y hp
+  * @param universo universo al que pertenece luchador de Marvel
+  * @param frase frase que dice el luchador de Marvel
   */
- export class Pokemon extends Fighter {
-   /**
-    * Constructor de la clase Pokemon
-    * @param nombre nombre del Pokemon
-    * @param peso peso del Pokemon
-    * @param altura altura del Pokemon
-    * @param datos datos de ataque, velocidad, defensa y hp
-    * @param universo universo al que pertenece el Pokemon
-    * @param frase frase que dice el Pokemon
-    */
-   constructor(nombre: string, peso: number, altura: number, datos: DatosBasicos, universo: Universo, frase: string) {
-     super(nombre, peso, altura, datos, universo, frase);
-   }
- }
+  constructor(nombre: string, peso: number, altura: number, datos: DatosBasicos, universo: Universo, frase: string) {
+    super(nombre, peso, altura, datos, universo, frase);
+  }
+}
