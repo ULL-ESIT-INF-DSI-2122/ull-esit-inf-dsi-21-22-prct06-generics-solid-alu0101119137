208e9dcb5a0971a28fcc6a66c950c7b5949fecee
Andrea Hernandez <alu0101119137@ull.edu.es>
2022-03-24 11:32:03 +0000

    interfaces ej 2

---
+++ src/ejercicio-2/interfaces.ts
@@ -0,0 +1,32 @@
+/**
+ * Interfaz para una coleccion de emisiones
+ */
+export interface Streamable<T> {
+  /**
+   * Array generico de coleccion de emisiones
+   */
+  coleccion: T[];
+
+  /**
+   * Método que añade un programa a la colección
+   * @param programa Programa que añadir
+   */
+  add(programa: T): void;
+
+  /**
+   * Método que retorna una colección de emisiones
+   */
+  getColeccion(): T[];
+}
+
+/**
+ * Interfaz de búsqueda
+ */
+export interface Search<T> {
+  /**
+   * Busca los elementos según un atributo y un tipo
+   * @param atributo Atributo a buscar
+   * @param tipo Tipo de atributo
+   */
+  buscar(atributo: string, tipo: string): T[];
+}
